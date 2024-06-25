d02e87fe1ee03f9c3208db0b492e9d128ee14feb
jwj <jwjang@mf.seegene.com>
2024-06-25 15:03:31 +0900

    오류 수정

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
-import { menu } from './menu.js'
-import { port } from './port.js'
-import { link } from './link.js'
-import { smooth } from './smooth.js'
+import { menu } from "./menu.js";
+import { port } from "./port.js";
+import { link } from "./link.js";
+import { smooth } from "./smooth.js";
 
 window.addEventListener("load", function(){
-    smooth();
-    link();
     menu();
     port();
-})
+    link();
+    smooth();
+});
